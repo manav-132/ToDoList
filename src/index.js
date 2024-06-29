83dd57fc5c8a15e41359f2f5257361cda9998704
manav-132 <103658463+manav-132@users.noreply.github.com>
2024-06-29 17:22:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from './redux/store';
 import App from './App';
 import './index.css';
 
+// Render the App component within the Redux Provider to enable state management
 ReactDOM.render(
   <Provider store={store}>
     <App />
