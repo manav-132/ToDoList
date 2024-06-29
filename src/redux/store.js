e75ba77550c445f2018d72f65fe710086094a836
manav-132 <103658463+manav-132@users.noreply.github.com>
2024-06-29 17:19:31 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 import taskReducer from './reducer';
 
+// Create a Redux store
 const store = createStore(
   taskReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
