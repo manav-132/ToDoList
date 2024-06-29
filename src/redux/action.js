e9bb607f9514a80edb016f08411140276d17de77
manav-132 <103658463+manav-132@users.noreply.github.com>
2024-06-29 17:18:43 +0530

    Update action.js

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,8 +1,10 @@
+// Action types
 export const ADD_TASK = 'ADD_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
 export const EDIT_TASK = 'EDIT_TASK';
 export const TOGGLE_TASK = 'TOGGLE_TASK';
 
+// Action creators
 export const addTask = (task) => ({
   type: ADD_TASK,
   payload: task,
